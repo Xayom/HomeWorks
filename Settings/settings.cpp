c782b6bbf33059c2170fe7457c0386c72407b14b
Xayom <xayom96@mail.ru>
2015-05-07 23:00:55 +0300

    settings

---
+++ Settings/settings.cpp
@@ -0,0 +1,301 @@
+#include "settings.h"
+
+settings::param::param(const std::string & name, std::string value, settings *parent, bool empty)
+{
+    this->value = value;
+    this->name = name;
+    this->parent = parent;
+    isBlank = empty;
+}
+
+settings::param::operator int() const
+{
+    if (isBlank)
+    {
+        throw EmptyProperty(name);
+    }
+    size_t *pos = new size_t;
+    int amont = stoi(value, pos);
+    size_t *posd = new size_t;
+    double amontd = stod(value, posd);
+    if (*pos != value.length() && *posd != value.length()){
+        throw std::invalid_argument(value);
+    }
+    return amont;
+}
+
+settings::param::operator std::string() const
+{
+    if (isBlank)
+        throw EmptyProperty(name);
+    return value;
+}
+
+settings::param::operator double() const
+{
+    if (isBlank)
+        throw (name);
+    size_t *pos = new size_t;
+    double amont = stod(value, pos);
+    if (*pos != value.length())
+        throw std::invalid_argument(value);
+    return amont;
+}
+
+settings::param::operator bool() const
+{
+    if (isBlank)
+        throw (name);
+    bool result;
+    if (value == "true" || value == "1")
+        result = true;
+    else if (value == "false" || value == "0")
+        result = false;
+    else
+        throw std::invalid_argument(value);
+    return result;
+}
+
+settings::param &settings::param::operator=(const string &newElement)
+{
+    value = newElement;
+    isBlank = false;
+    parent->synch(name, value);
+    return *this;
+}
+
+settings::param &settings::param::operator=(const char *newElement)
+{
+    value = std::string(newElement);
+    isBlank = false;
+    parent->synch(name, value);
+    return *this;
+}
+
+settings::param &settings::param::operator=(int newElement)
+{
+    value = std::to_string(newElement);
+    isBlank = false;
+    parent->synch(name, value);
+    return *this;
+}
+
+settings::param &settings::param::operator=(bool newElement)
+{
+    value = newElement?"true":"false";
+    isBlank = false;
+    parent->synch(name, value);
+    return *this;
+}
+
+settings::param &settings::param::operator=(double newElement)
+{
+    value = std::to_string(newElement);
+    isBlank = false;
+    parent->synch(name, value);
+    return *this;
+}
+
+settings::param &settings::param::operator+=(const string &newElement)
+{
+    if (isBlank)
+        throw (name);
+    value += newElement;
+    parent->synch(name, value);
+    return *this;
+}
+
+settings::param &settings::param::operator+=(const char *newElement)
+{
+
+    if (isBlank)
+        throw (name);
+    value += std::string(newElement);
+    parent->synch(name, value);
+    return *this;
+}
+
+settings::param &settings::param::operator+=(int newElement)
+{
+    if (isBlank)
+        throw (name);
+    value = std::to_string(this->operator int() + newElement);
+    parent->synch(name, value);
+    return *this;
+}
+
+settings::param &settings::param::operator+=(double newElement)
+{
+    if (isBlank)
+        throw (name);
+    value = std::to_string(this->operator double() + newElement);
+    parent->synch(name, value);
+    return *this;
+}
+
+settings::param &settings::param::operator-=(int newElement)
+{
+    if (isBlank)
+        throw EmptyProperty(name);
+    value = std::to_string(this->operator int() - newElement);
+    parent->synch(name, value);
+    return *this;
+}
+
+settings::param &settings::param::operator*=(int newElement)
+{
+    if (isBlank)
+        throw EmptyProperty(name);
+    value = std::to_string(this->operator int() * newElement);
+    parent->synch(name, value);
+    return *this;
+}
+
+settings::param &settings::param::operator-=(double newElement)
+{
+    if (isBlank)
+        throw EmptyProperty(name);
+    value = std::to_string(this->operator double() - newElement);
+    parent->synch(name, value);
+    return *this;
+}
+
+settings::param &settings::param::operator/=(int newElement)
+{
+    if (isBlank)
+        throw EmptyProperty(name);
+    value = std::to_string(this->operator int() / newElement);
+    parent->synch(name, value);
+    return *this;
+}
+
+settings::param &settings::param::operator*=(double newElement)
+{
+    if (isBlank)
+        throw EmptyProperty(name);
+    value = std::to_string(this->operator double() * newElement);
+    parent->synch(name, value);
+    return *this;
+}
+
+settings::param &settings::param::operator/=(double newElement)
+{
+    if (isBlank)
+        throw EmptyProperty(name);
+    value = std::to_string(this->operator double() / newElement);
+    parent->synch(name, value);
+    return *this;
+}
+
+settings::param &settings::param::operator|=(bool newElement)
+{
+    if (isBlank)
+        throw EmptyProperty(name);
+    value = std::to_string(this->operator bool() || newElement);
+    parent->synch(name, value);
+    return *this;
+}
+
+bool settings::param::is_empty() const
+{
+    return isBlank;
+}
+
+
+settings::settings(const std::string &filename)
+{
+    if (!std::ifstream(filename).good()){
+        std::ofstream(filename);
+    }
+    this->filename = filename;
+    reload();
+}
+
+settings::param &settings::param::operator&=(bool newElement)
+{
+    if (isBlank)
+        throw EmptyProperty(name);
+    value = std::to_string(this->operator bool() && newElement);
+    parent->synch(name, value);
+    return *this;
+}
+
+const std::string &settings::get(const std::string &name, const std::string &def) const
+{
+    try {
+        return list.at(name);
+    }
+    catch (std::out_of_range) {
+        return def;
+    }
+}
+
+void settings::set(const std::string &name, const std::string &value)
+{
+    list[name] = value;
+    synch();
+}
+
+void settings::reset()
+{
+    std::ofstream(filename, std::ofstream::trunc);
+    list.clear();
+}
+
+void settings::reload()
+{
+    list.clear();
+    ifstream in(filename);
+    string current;
+    while (getline(in, current)){
+        size_t pos = current.find("=\"");
+        string name = current.substr(0, pos);
+        size_t lpos = current.find_last_of("\"");
+        string value = current.substr(pos + 2, lpos - (pos + 2));
+        list[name] = value;
+    }
+}
+
+void settings::synch()
+{
+    std::ofstream out(filename);
+    for (auto it = list.begin(); it != list.end(); it++){
+        std::string val = it->second;
+        out << it->first << "=\"" << val << "\"" << std::endl;
+    }
+    out.close();
+}
+
+settings::param settings::operator[](const string &name)
+{
+    bool empty = false;
+    string result = "";
+    try
+    {
+        result = list.at(name);
+    }
+    catch (std::out_of_range) {
+        empty = true;
+    }
+    return param(name, result, this, empty);
+}
+
+void settings::synch(string & name, string & value)
+{
+    list[name] = value;
+    synch();
+}
+
+const settings::param settings::operator[](const string &name) const
+{
+    bool empty = false;
+    string result = "";
+    try {
+        result = list.at(name);
+    }
+    catch (std::out_of_range) {
+        empty = true;
+    }
+    return param(name, result, this, empty);
+}
+
---
+++ Settings/settings.h
@@ -0,0 +1,248 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include <exception>
+#include <fstream>
+#include <stdexcept>
+#include <iostream>
+#include <map>
+#include <string>
+
+class EmptyProperty  : public std::exception{
+    public:
+        EmptyProperty(const std::string & name)throw():error_message("Trying to cast empty property: " + name + "."){}
+        virtual const char* what()const throw(){return error_message.c_str();}
+        ~EmptyProperty()throw(){}
+    private:
+        std::string error_message;
+};
+
+class settings {
+    public:
+        class param {
+            friend class settings;
+            private:
+                param(param const &);
+                param(const std::string & name, std::string value, settings *parent, bool empty = false);
+                std::string value;
+                std::string name;
+                bool isBlank;
+                settings *parent;
+            public:
+                /**
+                 * Cast value to std::string
+                 * @throw EmptyProperty
+                 * @throw std::invalid_argument
+                 */
+                operator std::string() const;
+
+                /**
+                 * Cast value to int
+                 * @throw EmptyProperty
+                 * @throw std::invalid_argument
+                 */
+                operator int() const;
+
+                /**
+                 * Cast value to bool
+                 * @throw EmptyProperty
+                 * @throw std::invalid_argument
+                 */
+                operator bool() const;
+
+                /**
+                 * Cast value to double
+                 * @throw EmptyProperty
+                 * @throw std::invalid_argument
+                 */
+                operator double() const;
+
+                /**
+                 * Assigns the given std::string to this value and returns a reference to this value.
+                 * @return reference to this
+                 */
+                param & operator=(std::string const &);
+
+                /**
+                 * Assigns the given char* to this value and returns a reference to this value.
+                 * @return reference to this
+                 */
+                param & operator=(const char*);
+
+                /**
+                 * Assigns the given int to this value and returns a reference to this value.
+                 * @return reference to this
+                 */
+                param & operator=(int);
+
+                /**
+                 * Assigns the given bool to this value and returns a reference to this value.
+                 * @return reference to this
+                 */
+                param & operator=(bool);
+
+                /**
+                 * Assigns the given double to this value and returns a reference to this value.
+                 * @return reference to this
+                 */
+                param & operator=(double);
+
+
+                /**
+                 * Appends the std::string onto the end of this value and returns a reference to this value.
+                 * @throw EmptyProperty
+                 * @return reference to this
+                 */
+                param & operator+=(std::string const &);
+
+                /**
+                 * Appends the char* onto the end of this value and returns a reference to this value.
+                 * @throw EmptyProperty
+                 * @return reference to this
+                 */
+                param & operator+=(const char*);
+
+                /**
+                 * Adds int to this value if this value can be casted to int.
+                 * @throw EmptyProperty
+                 * @throw std::invalid_argument
+                 * @return reference to this
+                 */
+                param & operator+=(int);
+
+                /**
+                 * Adds double to this value if this value can be casted to double.
+                 * @throw EmptyProperty
+                 * @throw std::invalid_argument
+                 * @return reference to this
+                 */
+                param & operator+=(double);
+
+
+                /**
+                 * Subtracts int of this value if this value can be casted to int.
+                 * @throw EmptyProperty
+                 * @throw std::invalid_argument
+                 * @return reference to this
+                 */
+                param & operator-=(int);
+
+                /**
+                 * Subtracts double of this value if this value can be casted to double.
+                 * @throw EmptyProperty
+                 * @throw std::invalid_argument
+                 * @return reference to this
+                 */
+                param & operator-=(double);
+
+
+                /**
+                 * Multiplies int by this value if this value can be casted to int.
+                 * @throw EmptyProperty
+                 * @throw std::invalid_argument
+                 * @return reference to this
+                 */
+                param & operator*=(int);
+
+                /**
+                 * Multiplies double of this value if this value can be casted to double.
+                 * @throw EmptyProperty
+                 * @throw std::invalid_argument
+                 * @return reference to this
+                 */
+                param & operator*=(double);
+
+
+                /**
+                 * Divides this value by int if this value can be casted to int.
+                 * @throw EmptyProperty
+                 * @throw std::invalid_argument
+                 * @return reference to this
+                 */
+                param & operator/=(int);
+
+                /**
+                 * Divides this value by double if this value can be casted to double.
+                 * @throw EmptyProperty
+                 * @throw std::invalid_argument
+                 * @return reference to this
+                 */
+                param & operator/=(double);
+
+
+                /**
+                 * Assigns given value and this value if this value can be casted to bool.
+                 * @throw EmptyProperty
+                 * @throw std::invalid_argument
+                 * @return reference to this
+                 */
+                param & operator|=(bool);
+
+                /**
+                 * Assigns given value or this value if this value can be casted to bool.
+                 * @throw EmptyProperty
+                 * @throw std::invalid_argument
+                 * @return reference to this
+                 */
+                param & operator&=(bool);
+
+                /**
+                 * Returns false if this property is empty otherwise returns true.
+                 */
+                bool is_empty() const;
+        };
+
+        // Main functions
+
+        friend class param;
+
+        /**
+         * Construct settings store
+         * and load data from file (if exists)
+         * \param filename Path to file with settings
+         */
+        settings(std::string const & filename);
+        /**
+         * Get setting value
+         * \param name Setting unique identifier
+         * \param def Default setting value
+         * \return Stored value for given name or default value
+         */
+        std::string const & get(std::string const & name,
+            std::string const & def = "") const;
+        /**
+         * Set or replace setting value and save changes to file
+         * \param name Setting unique identifier
+         * \param value New setting value
+         */
+        void set(std::string const & name,
+            std::string const & value);
+        /**
+         * Reset all settings
+         */
+        void reset();
+        /**
+         * Reload all settings from file
+         */
+        void reload();
+
+        // Advanced funсtions
+
+        /**
+          * Get constant setting wrapper
+          * \param name Setting unique identifier
+          */
+        const param operator[](std::string const & name) const;
+        /**
+          * Get constant setting wrapper
+          * \param name Setting unique identifier
+          */
+        param operator[](std::string const & name);
+
+private:
+        void synch(std::string &name, std::string &value);
+        void synch();
+        std::string filename;
+        std::map<std::string, std::string> list;
+};
+#endif // SETTINGS_SETTINGS_H
